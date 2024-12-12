94070f6316f634d785a51b515fbfb4e03bf7745a
Christophe Carrez <ccarrez@gmail.com>
2024-12-12 13:52:18 +0100

    initial commit

---
+++ Synergy_ASTM_driver.py
@@ -0,0 +1,95 @@
+import socket
+from datetime import datetime
+
+ENCODING = 'latin-1' # ou ascii...
+
+STX = '\x02' # Message start token
+ETX = '\x03' # Message end token
+EXT5B = '\x035B'
+EOT = '\x04' # ASTM session termination token
+ENQ = '\x05' # ASTM session initialization token
+ACK = '\x06' # Command accepted token
+NAK = '\x15' # Command rejected token
+ETB = '\x17' # Message chunk end token
+ETB37 = '\x1737'
+LF = '\x0A' # CR + LF shortcut
+CR = '\x0D' # CR + LF shortcut
+CRLF = CR + LF # CR + LF shortcut
+RECORD_SEP = '\x0D' # \r # Message records delimiter
+FIELD_SEP = '\x7C' # | # Record fields delimiter
+REPEAT_SEP = '\x5C' # \ # Delimeter for repeated fields
+COMPONENT_SEP = '\x5E' # ^ # Field components delimiter
+ESCAPE_SEP = '\x26' # & # Date escape token
+
+def cleanStr(data):
+    output=""
+    skip_next = False
+    # Iterate through each character in the string
+    for i in range(len(data)):
+        if skip_next:
+            # Skip this character and reset the flag
+            skip_next = False
+            continue
+        if data[i] == STX:
+            # Skip it and the next character
+            skip_next = True
+        else:
+            # Otherwise, add the character to the result
+            output += data[i]
+    return output.replace(EXT5B,'').replace(ETB37,'').replace(ENQ,'').replace(ACK,'').replace(ETB,'').replace(ETX,'').replace(EOT,'').rstrip()
+
+def handle_astm_message(message):
+    # return an ACK
+    if(message != EOT):
+        return ACK
+    return EOT
+
+def start_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print(f"Server listening on {host}:{port}")
+
+    while True:
+        try:
+            client_socket, addr = server_socket.accept()
+            print(f"Connection from {addr}")
+            handle_client(client_socket)
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            client_socket.close()
+
+def handle_client(client_socket):
+    count = 0
+    content = ""
+    try:
+        while True:
+            data = client_socket.recv(1024).decode(ENCODING)
+            if not data:
+                break
+            response = handle_astm_message(data)
+            if response != EOT:
+                    client_socket.send(response.encode(ENCODING))
+                    content += cleanStr(data)
+                    count += 1
+            else:
+                if count > 2:
+                    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+                    filename = f"SYNERGY_results_{current_datetime}.astm"
+                    f = open(filename, "a") #append
+                    f.write(content)
+                    f.close()
+                content = ""
+                count = 0
+    except ConnectionResetError:
+        print("Client disconnected abruptly")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client_socket.close()
+        print("Client connection closed")
+
+if __name__ == "__main__":
+    start_server('148.196.146.237', 3121)
